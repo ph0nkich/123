e617c74cf042ad3472d04d452eed108af1db41af
ph0nky <earthphonky@gmail.ru>
2025-09-30 23:13:53 +0300

    togglable is faceup

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -20,18 +20,21 @@ struct ContentView: View {
     }
 }
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
+    
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundStyle(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.foregroundStyle(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
